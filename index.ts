ba91d0b28aa0f94881580a97921a56597aa34670
SCP Foundation ID <jevringarmindo@gmail.com>
2025-04-14 16:28:51 +0700

    updated

--- index.ts
+++ index.ts
@@ -1,4 +1,11 @@
 import { bot } from './callback';
+import express from 'express';
+
+const app = express();
 
 bot.start({ drop_pending_updates: true });
 console.log('BOT STARTED!');
+
+app.listen(8000, () => {
+    console.log(`Sever listening on port 8000`);
+})
