484e870bf7e83c1e091e038096ef4026a34be85a
SCP Foundation ID <jevringarmindo@gmail.com>
2025-04-15 18:37:57 +0700

    updated

--- index.ts
+++ index.ts
@@ -2,6 +2,7 @@ import { bot } from './callback';
 import express from 'express';
 import { Server } from 'socket.io';
 import http from 'http';
+import { Request, Response } from 'express';
 
 const app = express();
 const server = http.createServer(app);
@@ -19,6 +20,11 @@ bot.start({ drop_pending_updates: true });
 console.log('BOT STARTED!');
 
 const port = process.env.PORT || 8000;
+
+app.get('/ping', (req: Request, res: Response) => {
+    res.status(200).json({ ok: true, message: 'Pong!' });
+    return;
+})
 
 server.listen(port, () => {
     console.log(`Server is running on port ${port}`);
